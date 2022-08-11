25cde6dfe3f1da3211eb6a6fdb01f6641dfa2e04
27LUlU <100126195+27LUlU@users.noreply.github.com>
2022-08-11 11:24:22 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import "./styles.css";
+
+function amIWilson(p) {
+  // check if prime is Wilson
+  if (p <= 0 || p === 1) {
+    return false;
+  } else if (p <= 3) {
+    return true;
+  } else {
+    var i = 1;
+    let j = p - 1;
+    while (j) {
+      i = i * j;
+      j--;
+    }
+    // return i;
+    // var j = 1;
+    // for (let i = 1; i <= p - 1; i++) {
+    //   j = j * i;
+    // }
+    if ((i + 1) % (p * p) === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+document.write(amIWilson(5));
+// document.getElementById("app").innerHTML = amIWilson(5);
